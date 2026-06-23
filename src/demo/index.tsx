5a9c0119ff56273ae106a705066c787c95ede7cd
MaxKrch <max.kyrchanov@gmail.com>
2026-06-23 15:38:17 +0500

    feat: Поправлен фавикон

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from 'demo/App';
 import './styles/global.scss';
-import './assets/favicon.png';
 
 const root = createRoot(document.getElementById('app')!);
 root.render(<App />)
